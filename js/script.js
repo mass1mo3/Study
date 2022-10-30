c25308f032be12b6b7c9e29c0d597a885e2dc461
Massimo <example@gmail.com>
2022-10-30 15:36:17 +0200

    firs console app

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt("Сколько фильмов Вы уже просмотрели?", "");
+
+const personalMovieDB = {
+    count:numberOfFilms, 
+    movies:{}, 
+    actors:{}, 
+    genres:[], 
+    private: false
+}; 
+
+const a = prompt("Один из поседних просмотренных фильмов?", ""), 
+    b = prompt("На сколько его оцените?", ""), 
+    c = prompt("Один из поседних просмотренных фильмов?", ""), 
+    d = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
