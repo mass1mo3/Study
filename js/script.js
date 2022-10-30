ce5655b5c099ba8d908e02bc582622b8d84a8942
Massimo <example@gmail.com>
2022-10-30 12:45:20 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ function getAverageAge(users) {
     return users.reduce((prev, user) => prev + user.age, 0)/ users.length;
 }
 let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 29 };
+let petya = { name: "Петя", age: 12 };
+let masha = { name: "Маша", age: 45 };
 
 let arr = [ vasya, petya, masha ];
 
